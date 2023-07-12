40cff7cafdfcbdb48f1875f0d2d9ba4ddcf0eb48
sanders2022 <shnaideralex@gmail.com>
2023-07-12 22:51:10 +0300

    solution

---
+++ ubuntu/ubuntu2-1/cqlshscript.sh
@@ -0,0 +1,6 @@
+cqlsh --cqlshrc /root/cqlshrc  -f './commands.sql'   192.168.1.200
+cqlsh --cqlshrc /root/cqlshrc  -f './commands.sql'   192.168.1.201 
+cqlsh --cqlshrc /root/cqlshrc  -f './commands.sql'   192.168.1.202 
+ 
+
+
